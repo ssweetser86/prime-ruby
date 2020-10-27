93d6442103ac62380ed71a05db74bed5c5ae6fd7
Stephen <stephenjsweets@gmail.com>
2020-10-27 19:53:44 -0400

    commit tree c69927916df2386ee176dfef6f7e8a297c4998b3

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
 # Add  code here!
+def prime?(num)
+    i = 2
+    return false if num <= 1
+    return true if num < 4
+    while i < num
+        return false if num % i == 0
+        i += 1
+    end
+    return true
+end
